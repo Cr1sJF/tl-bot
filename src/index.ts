25e5ec0ff7c35b162d3334633b6b4de39aa298c0
Cristian <cristianjfarias2014@gmail.com>
2024-03-17 17:51:48 -0300

    ADD vercel config

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import express from "express";
-import "dotenv/config";
-import "reflect-metadata";
-import loader from "./loaders";
+import express from 'express';
+import 'dotenv/config';
+import 'reflect-metadata';
+import loader from './loaders';
 
 const app = express();
 
 loader(app);
+
+export default app;
