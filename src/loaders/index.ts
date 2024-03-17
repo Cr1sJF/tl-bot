7e23772d52297dd2b6f5d064e86558c85efa9862
Cristian <cristianjfarias2014@gmail.com>
2024-03-17 20:42:36 -0300

    ADD interval

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,9 +1,10 @@
-import { Application } from "express";
-import RoutesLoader from "./routes";
-import DataBase from "./db";
-import Logger from "../models/Loggers/Logger";
+import { Application } from 'express';
+import RoutesLoader from './routes';
+import DataBase from './db';
+import Logger from '../models/Loggers/Logger';
+import axios from 'axios';
 
-const log = new Logger("INDEX");
+const log = new Logger('INDEX');
 
 export default async (app: Application) => {
   try {
@@ -15,8 +16,24 @@ export default async (app: Application) => {
     app
       .listen(port, () => {
         log.info(`✅ SERVER RUNNING ON PORT ${port}`);
+
+        setInterval(async () => {
+          try {
+            log.info('PING...');
+            const data = await axios.get(
+              'https://tl-bot-g5x5.onrender.com/utils/ping',
+              {
+                timeout: 0,
+              }
+            );
+
+            log.info(data.data);
+          } catch (error: any) {
+            log.error("Error in ping", error);
+          }
+        }, 30000);
       })
-      .on("error", (err) => {
+      .on('error', (err) => {
         log.error(err);
         process.exit(1);
       });
